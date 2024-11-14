90f65a4c5266be9cf770fcc1a94202c89081b8af
Juli Li <uqjli47@uq.edu.au>
2024-11-14 15:02:24 +1000

    ma et al landscaoemetrics

--- pnr_fragmentation.R
+++ pnr_fragmentation.R
@@ -1,25 +1,40 @@
-install.packages("raster")
-install.packages("ncdf4")
-
+library(landscapemetrics)
 library(raster)
-library(ncdf4)
+
+# Load the forest cover raster (binary 10 m resolution)
+forest_cover <- raster("global_forest-watch_Tropical_Tree_Cover/10m_forest_cover/10N_110E.tif")
+
+# Check that the raster data is loaded correctly
+print(forest_cover)
+
+# Calculate fragmentation metrics on the entire 5 km² raster (10 m resolution)
+# You are calculating Edge Density (ED), Patch Density (PD), and Mean Patch Area (MPA)
+metrics <- calculate_lsm(
+  landscape = forest_cover,
+  what = c("lsm_c_ed", "lsm_c_pd", "lsm_c_area_mn")
+)
+
+# Convert results to a data frame for easy viewing and further analysis
+metrics_df <- as.data.frame(metrics)
+
+# Save the results to a CSV file
+write.csv(metrics_df, "R:/Chapter_3_fragmentation/global_forest-watch_Tropical_Tree_Cover/10m_forest_cover/fragmentation_metrics_5km2.csv")
+
+# View results
+print(metrics_df)
 
 
-nc_file <- "C:/Users/uqjli47/Downloads/9bc77f13bf50cbdddf34c6b44c299e27/2022.nc"
-
-# Open the NC file
-nc_data <- nc_open(nc_file)
-
-# Print the variable names
-print(nc_data$var)
-
-# Load the NetCDF file as a raster brick
-nc_raster <- brick(nc_file, varname = "lccs_class")  # Replace 'your_variable_name' with the variable you're interested in
-
-print(nc_raster)
 
 
-# Save as a GeoTIFF file
-writeRaster(nc_raster, filename = "2022LC.tif", format = "GTiff", overwrite = TRUE)
 
-getwd()
+
+
+
+
+
+
+
+
+
+
+
